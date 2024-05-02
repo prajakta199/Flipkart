bac884fdf2f8cfe5e8023ae1745cc01a3392ef56
Prajakta Pednekar <48241225+prajakta199@users.noreply.github.com>
2024-05-03 02:34:18 +0530

    Delete testng-browserstack-master directory

--- testng-browserstack-master/src/test/java/com/browserstack/pageobjects/HomePageLocators.java
+++
@@ -1,9 +0,0 @@
-package com.browserstack.pageobjects;
-
-public class HomePageLocators {
-
-	public static final String FLIPKART_ICON="//img[@title='Flipkart']";
-	public static final String SEARCH_BAR="//input[@placeholder='Search for Products, Brands and More']";
-	
-
-}
